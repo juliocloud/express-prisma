2d708e7a18c429faef0fe3c8282e301d1383c61d
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-14 11:36:46 -0300

    feat: main ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log('funciona')
+import { ApiExpress } from "./infra/api/express/api.express"
+import { CreateProductRoute } from "./infra/api/express/routes/product/create-product.express.route"
+import { ListProductRoute } from "./infra/api/express/routes/product/list-product.express.route"
+import { ProductRepositoryPrisma } from "./infra/respositories/product/product.repository.prisma"
+import { prisma } from "./package/prisma/prisma"
+import { CreateProductUseCase } from "./usecases/create-product/create-product.usecase"
+import { ListProductUsecase } from "./usecases/list-product/list-product.usecase"
+
+const main = () => {
+    const aRepo = ProductRepositoryPrisma.create(prisma)
+
+    const createProductUsecase = CreateProductUseCase.create(aRepo)
+    const listProductUsecase = ListProductUsecase.create(aRepo)
+
+    const createRoute = CreateProductRoute.create(createProductUsecase)
+    const listRoute = ListProductRoute.create(listProductUsecase)
+
+    const port = 8000
+
+    const api = ApiExpress.create([ createRoute, listRoute])
+
+    api.start(port)
+    
+}
+
+main()
